4410b970626e9b8cca45ac0c4528b1035117beef
holysyid <holysyid@gmail.com>
2022-11-22 04:52:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan5mobile/ListSucculentAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.pertemuan5mobile
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+
+class ListSucculentAdapter(private val listSucculent: ArrayList<Succulent>) :
+    RecyclerView.Adapter<ListSucculentAdapter.ListViewHolder>() {
+
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        var tvDetail: TextView = itemView.findViewById(R.id.tv_item_subname)
+        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listSucculent.size
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val succulent = listSucculent[position]
+
+        Glide.with(holder.itemView.context)
+            .load(succulent.picture)
+            .apply(RequestOptions().override(150, 150))
+            .into(holder.imgPhoto)
+
+        holder.tvName.text = succulent.name
+        holder.tvDetail.text = succulent.common_name
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/pertemuan5mobile/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.pertemuan5mobile
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var rvSucculent: RecyclerView
+    private var list: ArrayList<Succulent> = arrayListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvSucculent = findViewById(R.id.rv_id)
+        rvSucculent.setHasFixedSize(true)
+
+        list.addAll(SucculentData.listData)
+        showRecyclerList()
+    }
+    private fun showRecyclerList() {
+        rvSucculent.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListSucculentAdapter(list)
+        rvSucculent.adapter = listHeroAdapter
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+                showRecyclerList()
+            }
+
+            R.id.action_grid -> {
+
+            }
+            R.id.about_me -> {
+
+            }
+
+        }
+    }
+}
